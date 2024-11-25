559c216a8ec8442ed504e226f04bf440bc3bee84
abhay-krishna-udainiya <abhay.udainiya_cs.h24@gla.ac.in>
2024-11-25 23:20:55 +0530

    added the dictionary questions in the repositry

---
+++ coding-assignment/dict/dict_q03.py
@@ -0,0 +1,8 @@
+# 3. Merging Multiple Dictionaries Using update()
+
+# Problem: You need to merge multiple dictionaries into one, with the values from later dictionaries overwriting earlier ones.
+
+dict1 = {'name': 'Steve', 'age': 40}
+dict2 = {'age': 45, 'city': 'London'}
+dict1.update(dict2)  
+print(dict1)  
